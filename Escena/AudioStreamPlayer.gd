eda31bd363617ca8f247994cff5b58b10447f104
Nico <nico99rueda@gmail.com>
2026-04-03 22:55:42 +0200

    fix: corregir errores de inicio por fuente eliminada, variables sin usar y nodos inexistentes

--- Escena/AudioStreamPlayer.gd
+++ Escena/AudioStreamPlayer.gd
@@ -3,6 +3,6 @@ extends AudioStreamPlayer
 func _ready():
 	self.play()
 
-func _process(delta):
+func _process(_delta):
 	if self.playing == false:
 		self.play()
--- Escena/Olaia/HUD_PRINCIPAL.gd
+++ Escena/Olaia/HUD_PRINCIPAL.gd
@@ -1,11 +1,7 @@
 extends CanvasLayer
 
-onready var mouse = $clicked
-
-
 func _on_Settings_pressed():
 	$SettingsMenu.visible = true
-
 
 func _on_Start_pressed():
 	get_tree().change_scene("res://Escena/Olaia/Seleccio_Personatges.tscn")
